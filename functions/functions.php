d47243654aac306ce6e4cb7e875ef75e32f821ae
Elena <Elena-Bonito@yandex.ru>
2020-08-02 00:29:15 +0100

    добавила валидацию форме

--- functions/functions.php
+++ functions/functions.php
@@ -21,3 +21,47 @@ function calculate_time_to_expiration ($date_expiration) {
 
     return [$hours_expiration, $minutes_expiration];
 }
+
+function validate_lot_date ($input_date) {
+    $input_date = strtotime($input_date);
+    $current_date = time();
+    $diff = $input_date - $current_date;
+    if ($diff < 86400) {
+       return false;
+    }
+    return $input_date;
+    return true;
+}
+
+function translate($string)
+{
+    $replace = [
+        'а' => 'a', 'б' => 'b',
+        'в' => 'v', 'г' => 'g',
+        'д' => 'd', 'е' => 'e',
+        'ё' => 'yo', 'ж' => 'j',
+        'з' => 'z', 'и' => 'i',
+        'й' => 'y', 'к' => 'k',
+        'л' => 'l', 'м' => 'm',
+        'н' => 'n', 'о' => 'o',
+        'п' => 'p', 'р' => 'r',
+        'с' => 's', 'т' => 't',
+        'у' => 'u', 'ф' => 'f',
+        'х' => 'h', 'ц' => 'ts',
+        'ч' => 'ch', 'ш' => 'sh',
+        'щ' => 'sch', 'ъ' => '',
+        'ы' => 'i', 'ь' => '',
+        'э' => 'e', 'ю' => 'ju',
+        'я' => 'ja', ' ' => '-'
+    ];
+    $string = mb_strtolower($string, 'utf-8');
+
+    $string = strtr($string, $replace);
+
+    return
+        preg_replace('~[^a-z\-]~', null, $string);
+}
+
+function getPostVal($name) {
+    return $_POST[$name] ? $_POST[$name] : "";
+}
---
+++ templates/add_template.php
@@ -0,0 +1,73 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <?php foreach($categories as $val): ?>
+            <li class="nav__item">
+                <a href="all-lots.html"><?=htmlspecialchars($val['title']); ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
+
+<?php $classname = count($errors) ? 'form--invalid' : ""; ?>
+<form class="form form--add-lot container <?= $classname; ?>" action="add.php" method="post" enctype="multipart/form-data">
+<h2>Добавление лота</h2>
+    <div class="form__container-two">
+        <?php $classname = isset($errors['lot-name']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item  <?= $classname; ?>">
+            <label for="lot-name">Наименование <sup>*</sup></label>
+            <input id="lot-name" type="text" name="lot-name" value="<?=getPostVal('lot-name'); ?>" placeholder="Введите наименование лота">
+            <span class="form__error"><?= $errors['lot-name']; ?></span>
+        </div>
+        <?php $classname = isset($errors['category']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item  <?= $classname; ?>">
+            <label for="category">Категория <sup>*</sup></label>
+            <select id="category" name="category">
+                <option value="0">Выберите категорию</option>
+                <?php foreach($categories as $val): ?>
+                    <option value="<?= $val['id'] ?>" <?php if ($_POST['category'] == $val['id']){echo ' selected';}?>><?=htmlspecialchars($val['title']); ?></option>
+                <?php endforeach; ?>
+            </select>
+            <span class="form__error"><?= $errors['category']; ?></span>
+        </div>
+    </div>
+    <?php $classname = isset($errors['message']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item form__item--wide <?= $classname; ?>">
+        <label for="message">Описание <sup>*</sup></label>
+        <textarea id="message" name="message" placeholder="Напишите описание лота"><?=getPostVal('message'); ?></textarea>
+        <span class="form__error"><?= $errors['message']; ?></span>
+    </div>
+    <?php $classname = isset($errors['lot-rate']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item form__item--file form__item--invalid <?= $classname; ?>">
+        <label>Изображение <sup>*</sup></label>
+        <div class="form__input-file">
+            <input class="visually-hidden" type="file" name="lot-img" id="lot-img" value="">
+            <label for="lot-img">
+                Добавить
+            </label>
+            <span class="form__error"><?= $errors['lot-img']; ?></span>
+        </div>
+    </div>
+    <div class="form__container-three">
+        <?php $classname = isset($errors['lot-rate']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?> ">
+            <label for="lot-rate">Начальная цена <sup>*</sup></label>
+            <input id="lot-rate" type="text" name="lot-rate" value="<?=getPostVal('lot-rate'); ?>" placeholder="0">
+            <span class="form__error"><?= $errors['lot-rate']; ?></span>
+        </div>
+        <?php $classname = isset($errors['lot-step']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?>">
+            <label for="lot-step">Шаг ставки <sup>*</sup></label>
+            <input id="lot-step" type="text" name="lot-step" value="<?=getPostVal('lot-step'); ?>" placeholder="0">
+            <span class="form__error"><?= $errors['lot-step']; ?></span>
+        </div>
+        <?php $classname = isset($errors['lot-date']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item <?= $classname; ?>">
+            <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
+            <input class="form__input-date" id="lot-date" type="text" name="lot-date" value="<?=getPostVal('lot-date'); ?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <span class="form__error"><?= $errors['lot-date']; ?></span>
+        </div>
+    </div>
+    <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <button type="submit" class="button">Добавить лот</button>
+</form>
+
