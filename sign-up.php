545ea74f77127bc846fcac668a93d5cffbfeb436
Elena <Elena-Bonito@yandex.ru>
2020-08-11 16:51:52 +0100

    добавила число

---
+++ sign-up.php
@@ -0,0 +1,55 @@
+<?php
+require_once('settings.php');
+$categories = [];
+$ads = [];
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    print($error);
+    $page_content = include_template('error.php');
+}
+else {
+    $sql = 'SELECT title, character_code FROM categories';
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        $error = mysqli_error($link);
+        $page_content = include_template('error.php');
+    }
+
+    $sql = 'SELECT lots.title as lot_title, lots.id, base_price, img, closing_date as date_expiration, categories.title as lot_category, IFNULL(MAX(amount), lots.base_price) as price FROM lots
+            LEFT JOIN bets ON lots.id = bets.lotID
+            JOIN categories ON lots.categoryID = categories.id
+            WHERE closing_date > CURRENT_TIMESTAMP
+            GROUP BY lots.id
+            ORDER BY lots.date_time DESC';
+
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $ads = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $page_content = include_template('sign-up_template.php');
+    }
+    else {
+        $error = mysqli_error($link);
+        $page_content = include_template('error.php');
+    }
+}
+
+
+$is_auth = rand(0, 1);
+
+$user_name = 'Elena Sinko';
+
+$layout_content = include_template('layout.php', [
+    'content' => $page_content,
+    'categories' => $categories,
+    'title' => 'Главная',
+    'is_auth' => $is_auth,
+    'user_name' => $user_name
+]);
+
+print($layout_content);
+
--- templates/layout.php
+++ templates/layout.php
@@ -33,7 +33,7 @@
                 <?php else: ?>
                     <ul class="user-menu__list">
                         <li class="user-menu__item">
-                            <a href="#">Регистрация</a>
+                            <a href="pages/sign-up.html">Регистрация</a>
                         </li>
                         <li class="user-menu__item">
                             <a href="#">Вход</a>
